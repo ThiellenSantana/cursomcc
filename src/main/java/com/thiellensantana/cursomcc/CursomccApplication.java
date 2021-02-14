f7724ff24240ba324bc298e716702c797bda914e
Thiellen <thiellenaty@hotmail.com>
2021-02-14 20:09:15 -0300

    Projeto criado

---
+++ src/main/java/com/thiellensantana/cursomcc/CursomccApplication.java
@@ -0,0 +1,13 @@
+package com.thiellensantana.cursomcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiellensantana/cursomcc/CursomccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiellensantana.cursomcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
