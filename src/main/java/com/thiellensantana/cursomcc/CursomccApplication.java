77caaa6d48b58256055fececd9a39b45a57b4518
Thiellen <thiellenaty@hotmail.com>
2021-02-15 21:18:00 -0300

    Criando operação de instanciação

--- src/main/java/com/thiellensantana/cursomcc/CursomccApplication.java
+++ src/main/java/com/thiellensantana/cursomcc/CursomccApplication.java
@@ -1,13 +1,33 @@
 package com.thiellensantana.cursomcc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomccApplication {
+import com.thiellensantana.cursomcc.domain.Categoria;
+import com.thiellensantana.cursomcc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomccApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository; 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomccApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Categoria cat1 = new Categoria (null, "Informatica");
+		Categoria cat2 = new Categoria (null,"Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	
 	}
 
 }
